cbe8fe0088f0d551226f1f83b4cd73e4b38eb608
odeggs <118347348+odeggs@users.noreply.github.com>
2023-10-19 01:22:13 -0700

    Add files via upload

---
+++ Week-2/O.DANIEL.CSC102/Week-1/practice-1.rs
@@ -0,0 +1,12 @@
+fn main(){
+    printIn!("Welcome to COS 102")
+
+    printIn!("The course learning outcome is as follows:")
+
+    printIn!("1. Distinguish between computer and computer programming,/n
+              2. Develop  some techniques in computer science,/n
+              3. Understand the different area of study in computer science,/n
+              4. Navigate the career prospects in computer science./n
+              5. Conversant with computer programming concepts./n
+              6.Conversant with applications of computer science.")
+ 
